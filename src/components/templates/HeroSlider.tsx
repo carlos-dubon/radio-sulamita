f713b0b21dc9591054d85b3b3bb6043b05a09baa
Carlos Dubón <holacarlosdubon@gmail.com>
2022-01-16 18:40:04 -0600

    add first slide

--- src/components/templates/HeroSlider.tsx
+++ src/components/templates/HeroSlider.tsx
@@ -8,9 +8,11 @@ export const HeroSlider: FC = () => {
   return (
     <Swiper spaceBetween={50}>
       {/* <Swiper modules={[Autoplay]} spaceBetween={50} autoplay={{ delay: 6000 }}> */}
-      <SwiperSlide>
-        <div style={{ width: "100%", height: "820px", position: "relative" }}>
-          {" "}
+      <SwiperSlide className="relative" style={{ height: "820px" }}>
+        <div
+          className="w-full absolute top-0 left-0 -z-10"
+          style={{ height: "820px" }}
+        >
           <Image
             className="brightness-75"
             src="/slider/slide-1.jpg"
@@ -18,7 +20,22 @@ export const HeroSlider: FC = () => {
             layout="fill"
             objectFit="cover"
             priority={true}
-          ></Image>
+          />
+        </div>
+        <div className="flex justify-center items-center absolute top-0 left-0 h-full w-full">
+          <div className="container flex gap-3 flex-col">
+            <div className="bg-rs-primary px-3 py-2 text-base font-medium text-white w-fit mb-6">
+              Mateo 22:37-38
+            </div>
+            <div className="text-white font-medium text-2xl">
+              Amaras al señor tu Dios con todo
+            </div>
+            <div className="text-white font-extrabold text-6xl">tu corazón</div>
+            <div className="text-white font-medium text-base">
+              con toda tu alma y con toda tu mente. Este es el primero y grande
+              mandamiento
+            </div>
+          </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>

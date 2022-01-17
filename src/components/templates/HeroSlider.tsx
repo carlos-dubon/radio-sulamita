b5c70f5cab475a329f578bd9557217fdb4505570
Carlos Dubón <holacarlosdubon@gmail.com>
2022-01-16 19:43:17 -0600

    🌇 image slider

--- src/components/templates/HeroSlider.tsx
+++ src/components/templates/HeroSlider.tsx
@@ -6,15 +6,14 @@ import Image from "next/image";
 
 export const HeroSlider: FC = () => {
   return (
-    <Swiper spaceBetween={50}>
-      {/* <Swiper modules={[Autoplay]} spaceBetween={50} autoplay={{ delay: 6000 }}> */}
+    <Swiper modules={[Autoplay]} autoplay={{ delay: 6000 }}>
       <SwiperSlide className="relative" style={{ height: "820px" }}>
         <div
-          className="w-full absolute top-0 left-0 -z-10"
+          className="w-full relative top-0 left-0 -z-10"
           style={{ height: "820px" }}
         >
           <Image
-            className="brightness-75"
+            className="brightness-75 bg-left-bottom"
             src="/slider/slide-1.jpg"
             alt="slide-1"
             layout="fill"
@@ -24,7 +23,7 @@ export const HeroSlider: FC = () => {
         </div>
         <div className="flex justify-center items-center absolute top-0 left-0 h-full w-full">
           <div className="container flex justify-end px-28">
-            <div className="flex gap-3 flex-col">
+            <div className="flex gap-4 flex-col">
               <div className="bg-rs-primary px-3 py-2 text-base font-medium text-white w-fit mb-6">
                 Mateo 22:37-38
               </div>
@@ -34,7 +33,7 @@ export const HeroSlider: FC = () => {
               <div className="text-white font-extrabold text-6xl">
                 tu corazón
               </div>
-              <div className="text-white font-medium text-base">
+              <div className="text-white font-light text-base">
                 con toda tu alma y con toda tu mente. Este es el primero y
                 grande mandamiento
               </div>
@@ -42,9 +41,71 @@ export const HeroSlider: FC = () => {
           </div>
         </div>
       </SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+
+      <SwiperSlide className="relative" style={{ height: "820px" }}>
+        <div
+          className="w-full relative top-0 left-0 -z-10"
+          style={{ height: "820px" }}
+        >
+          <Image
+            className="brightness-75 bg-top"
+            src="/slider/slide-2.jpg"
+            alt="slide-2"
+            layout="fill"
+            objectFit="cover"
+            priority={true}
+          />
+        </div>
+        <div className="flex justify-center items-center absolute top-0 left-0 h-full w-full">
+          <div className="container flex px-28 gap-4 flex-col">
+            <div className="text-white font-medium text-2xl">
+              Transmitiendo desde la puerta del
+            </div>
+            <div className="text-white font-extrabold text-6xl">
+              mundo Maya,
+            </div>
+            <div className="text-white text-base font-light">
+              para el mundo entero. Somos Radio Sulamita, 90.1 FM — La
+              frecuencia del amor de Dios.
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+
+      <SwiperSlide className="relative" style={{ height: "820px" }}>
+        <div
+          className="w-full relative top-0 left-0 -z-10"
+          style={{ height: "820px" }}
+        >
+          <Image
+            className="brightness-75 bg-bottom"
+            src="/slider/slide-3.jpg"
+            alt="slide-3"
+            layout="fill"
+            objectFit="cover"
+            priority={true}
+          />
+        </div>
+        <div className="flex justify-center items-center absolute top-0 left-0 h-full w-full">
+          <div className="container flex justify-end px-28">
+            <div className="flex gap-4 flex-col">
+              <div className="bg-rs-primary px-3 py-2 text-base font-medium text-white w-fit mb-6">
+                Juan 14:2
+              </div>
+              <div className="text-white font-medium text-2xl">
+                En el hogar de mi Padre hay muchas
+              </div>
+              <div className="text-white font-extrabold text-6xl">
+                viviendas;
+              </div>
+              <div className="text-white font-light text-base">
+                si no fuera así, ya se lo habría dicho a ustedes. Voy a
+                prepararles un lugar.
+              </div>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
     </Swiper>
   );
 };

ea0ca97251462917fe80502118979b703009844c
Carlos Dubón <holacarlosdubon@gmail.com>
2022-01-20 17:10:45 -0600

    Update Videos.tsx

--- src/components/templates/Videos.tsx
+++ src/components/templates/Videos.tsx
@@ -10,17 +10,16 @@ const Videos: FC = () => {
     >
       <div className="grid grid-cols-2 justify-items-center">
         <div className="w-4/5">
-          <div className="mb-1">El diseño de la vida</div>
-          <div className="text-rs-primary text-sm">
+          <div className="mb-1 font-medium">El diseño de la vida</div>
+          <div className="text-rs-primary text-sm mb-2 h-10">
             ¿Existen pruebas de que la vida tenga un diseñador? Todos los seres
             vivos tienen algo en común que los relaciona entre sí.
           </div>
         </div>
         <div className="w-4/5">
-          <div className="mb-1">El diseño de la vida</div>
-          <div className="text-rs-primary text-sm">
-            ¿Existen pruebas de que la vida tenga un diseñador? Todos los seres
-            vivos tienen algo en común que los relaciona entre sí.
+          <div className="mb-1 font-medium">Haz sacrificios por otros</div>
+          <div className="text-rs-primary text-sm mb-2 h-10">
+            Demuestra tu amor poniendo a los demás en primer lugar.
           </div>
         </div>
       </div>

20421a8b73edd21f8ae7963b5c48620314da7d81
Carlos Dubón <holacarlosdubon@gmail.com>
2022-01-20 16:24:23 -0600

    add video section

--- src/components/templates/Videos.tsx
+++ src/components/templates/Videos.tsx
@@ -8,7 +8,22 @@ const Videos: FC = () => {
       subtitle="“Cada acontecimiento, grande y pequeño, es una parábola por medio de la cual Dios nos habla y el arte de vivir es recibir el mensaje.”"
       bibleVerse="2 Timoteo 3:16-17"
     >
-      Hello world
+      <div className="grid grid-cols-2 justify-items-center">
+        <div className="w-4/5">
+          <div className="mb-1">El diseño de la vida</div>
+          <div className="text-rs-primary text-sm">
+            ¿Existen pruebas de que la vida tenga un diseñador? Todos los seres
+            vivos tienen algo en común que los relaciona entre sí.
+          </div>
+        </div>
+        <div className="w-4/5">
+          <div className="mb-1">El diseño de la vida</div>
+          <div className="text-rs-primary text-sm">
+            ¿Existen pruebas de que la vida tenga un diseñador? Todos los seres
+            vivos tienen algo en común que los relaciona entre sí.
+          </div>
+        </div>
+      </div>
     </Section>
   );
 };

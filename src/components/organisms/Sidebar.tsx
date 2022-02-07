969ee888c4970461109034f60b27da4938dc53bd
Carlos Dubón <holacarlosdubon@gmail.com>
2022-02-07 14:52:27 -0600

    use swipe to close sidebar

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -1,10 +1,27 @@
-import { useAppSelector } from "@app/hooks";
+import { useAppSelector, useAppDispatch } from "@app/hooks";
 import { FC, useEffect, useState } from "react";
+import useSwipe from "@adrianbielawski/use-swipe";
+import { toggle } from "src/state/slices/sidebarSlice";
 
 const Sidebar: FC = () => {
   const openState: boolean = useAppSelector((state) => state.sidebar.open);
+  const dispatch = useAppDispatch();
 
   const [open, setOpen] = useState<boolean>(false);
+  const handleSwipe = (x: number, y: number) => {
+    console.log(x, y);
+  };
+
+  const handleSwipeEnd = (x: number, y: number, isQuick: boolean) => {
+    console.log(x, y, isQuick);
+
+    if(x < -40){
+        //close dispatch(toggle());
+        dispatch(toggle());
+    }
+  };
+
+  const swipeRef = useSwipe(handleSwipe, handleSwipeEnd);
 
   useEffect(() => {
     if (openState) {
@@ -14,14 +31,18 @@ const Sidebar: FC = () => {
       }, 300);
     } else {
       setOpen(false);
+      setTimeout(() => {
+        setOpen(false);
+      }, 300);
     }
   }, [openState]);
 
   return (
     <div
+      ref={swipeRef}
       className={`bg-white text-black fixed top-0 ${
         open ? "left-0" : "-left-full"
-      } z-[51] w-9/12 max-w-xs h-full flex shadow-xl transition-all duration-700`}
+      } z-[60] w-9/12 max-w-xs h-full flex shadow-xl transition-all duration-700`}
     >
       <div>Hello world</div>
     </div>

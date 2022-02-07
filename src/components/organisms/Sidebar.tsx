2a86de0bc973c2c029c168e274435d40b2538478
Carlos Dubón <holacarlosdubon@gmail.com>
2022-02-07 16:42:58 -0600

    add animation when sidebar opened

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -6,14 +6,26 @@ import {
   useSwipeable,
 } from "react-swipeable";
 import { toggle } from "src/state/slices/sidebarSlice";
+import { toggle as togglePlayer } from "src/state/slices/playerSlice";
 import Link from "next/link";
-import { faHandHoldingHeart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHandHoldingHeart,
+  faHeadphonesAlt,
+  faPauseCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Fade } from "react-awesome-reveal";
 
 const Sidebar: FC = () => {
   const dispatch = useAppDispatch();
 
   const openState: boolean = useAppSelector((state) => state.sidebar.open);
+
+  const playerState: boolean = useAppSelector((state) => state.player.playing);
+
+  const isAudioLoading: boolean = useAppSelector(
+    (state) => state.player.loading
+  );
 
   const [open, setOpen] = useState<boolean>(false);
 
@@ -65,32 +77,55 @@ const Sidebar: FC = () => {
         open ? "left-0" : "-left-full"
       } z-[60] w-9/12 max-w-xs h-full flex flex-col shadow-xl transition-all duration-700 py-3`}
     >
-      {links.map((link) => {
-        return (
-          <div key={link.target}>
-            <Link href={link.target}>
-              <a
-                className="w-full h-16 flex justify-start px-5 transition-all hover:bg-rs-primary hover:text-white duration-500 text-xs uppercase tracking-wider"
-                onClick={() => dispatch(toggle())}
-              >
-                {link.label}
-              </a>
-            </Link>
-          </div>
-        );
-      })}
-      <Link href="/#donar">
-        <a
-          className="w-full h-16 flex justify-start px-5 transition-all hover:bg-rs-secondary hover:text-white duration-500 text-xs uppercase tracking-wider"
-          onClick={() => dispatch(toggle())}
+      <Fade duration={450} delay={300}>
+        {links.map((link) => {
+          return (
+            <div key={link.target}>
+              <Link href={link.target}>
+                <a
+                  className="w-full h-16 flex justify-start px-5 transition-all hover:bg-rs-primary hover:text-white duration-500 text-xs uppercase tracking-wider"
+                  onClick={() => dispatch(toggle())}
+                >
+                  {link.label}
+                </a>
+              </Link>
+            </div>
+          );
+        })}
+
+        <Link href="/#donar">
+          <a
+            className="w-full h-16 flex justify-start px-5 transition-all hover:bg-rs-secondary hover:text-white duration-500 text-xs uppercase tracking-wider"
+            onClick={() => dispatch(toggle())}
+          >
+            Donar
+            <FontAwesomeIcon
+              icon={faHandHoldingHeart}
+              className={"group-hover:text-white w-4 ml-3"}
+            />
+          </a>
+        </Link>
+
+        <button
+          className="w-full h-16 flex justify-start items-center px-5 transition-all bg-rs-primary hover:bg-rs-secondary text-white duration-500 text-xs uppercase tracking-wider"
+          onClick={() => {
+            dispatch(toggle());
+            dispatch(togglePlayer());
+          }}
         >
-          Donar
-          <FontAwesomeIcon
-            icon={faHandHoldingHeart}
-            className={"group-hover:text-white w-4 ml-3"}
-          />
-        </a>
-      </Link>
+          {isAudioLoading
+            ? "Cargando..."
+            : playerState
+            ? "Pausar"
+            : "Reproducir"}
+          {!isAudioLoading ? (
+            <FontAwesomeIcon
+              icon={playerState ? faPauseCircle : faHeadphonesAlt}
+              className="w-4 ml-3"
+            />
+          ) : null}
+        </button>
+      </Fade>
     </div>
   );
 };

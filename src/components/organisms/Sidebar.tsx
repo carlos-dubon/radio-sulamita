944b5dbe3a0cb2dcb7328779b60c188e754fa6a7
Carlos Dubón <holacarlosdubon@gmail.com>
2022-02-07 16:16:01 -0600

    use react-swipeable

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -1,10 +1,24 @@
-import { useAppSelector } from "@app/hooks";
+import { useAppSelector, useAppDispatch } from "@app/hooks";
 import { FC, useEffect, useState } from "react";
+import {
+  SwipeableHandlers,
+  SwipeEventData,
+  useSwipeable,
+} from "react-swipeable";
+import { toggle } from "src/state/slices/sidebarSlice";
 
 const Sidebar: FC = () => {
+  const dispatch = useAppDispatch();
+
   const openState: boolean = useAppSelector((state) => state.sidebar.open);
 
   const [open, setOpen] = useState<boolean>(false);
+
+  const handlers: SwipeableHandlers = useSwipeable({
+    onSwipedLeft: (eventData: SwipeEventData) => {
+      dispatch(toggle());
+    },
+  });
 
   useEffect(() => {
     if (openState) {
@@ -21,6 +35,7 @@ const Sidebar: FC = () => {
 
   return (
     <div
+      {...handlers}
       className={`bg-white text-black fixed top-0 ${
         open ? "left-0" : "-left-full"
       } z-[60] w-9/12 max-w-xs h-full flex shadow-xl transition-all duration-700`}

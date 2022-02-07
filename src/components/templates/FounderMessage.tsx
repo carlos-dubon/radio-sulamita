1fb94d017cfa1dcd714aea66985f172d67c72236
Carlos Dubón <holacarlosdubon@gmail.com>
2022-02-06 22:05:59 -0600

    🎥 added more animations

--- src/components/templates/FounderMessage.tsx
+++ src/components/templates/FounderMessage.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { SectionQuote } from "@lib/molecules";
 import carlos_dubon from "public/carlos-dubon.jpg";
 import { useBreakpoint } from "@app/hooks";
+import { Fade } from "react-awesome-reveal";
 
 const FounderMessage: FC = () => {
   const tailwind_sm: boolean = useBreakpoint("sm");
@@ -27,18 +28,18 @@ const FounderMessage: FC = () => {
           />
         </div>
         <div className="flex flex-col justify-center md:max-w-[555px] px-7 py-24 md:p-14">
-          <div className="text-3xl mb-1">Carlos Dubón</div>
-          <div className="text-stone-600 mb-6">
-            Ex-Director General y Fundador
-          </div>
-          <div className="text-stone-600 leading-7 text-sm">
-            “Radio Sulamita 90.1 FM, es una emisora evangélica destinada a
-            difundir un mensaje de cambio que permita la restauración,
-            edificación, renovación y salvación fundamentada en la palabra de
-            Dios. Con una programación variada y bien nutrida, emite su señal
-            las veinticuatro horas del día, llegando a casas y lugares
-            recónditos del planeta a través de su señal online.”
-          </div>
+          <Fade cascade duration={400} direction="up" triggerOnce>
+            <div className="text-3xl mb-1">Carlos Dubón</div>
+            <div className="text-stone-600">Ex-Director General y Fundador</div>
+            <div className="text-stone-600 leading-7 text-sm mt-6">
+              “Radio Sulamita 90.1 FM, es una emisora evangélica destinada a
+              difundir un mensaje de cambio que permita la restauración,
+              edificación, renovación y salvación fundamentada en la palabra de
+              Dios. Con una programación variada y bien nutrida, emite su señal
+              las veinticuatro horas del día, llegando a casas y lugares
+              recónditos del planeta a través de su señal online.”
+            </div>
+          </Fade>
         </div>
       </div>
       <SectionQuote

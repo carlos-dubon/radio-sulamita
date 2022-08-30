c1123b42ccc85d2054358505babc1e23ca925fa0
Carlos Dubón <holacarlosdubon@gmail.com>
2022-08-30 12:39:40 -0600

    add .env

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { FirebaseApp, initializeApp } from "firebase/app";
 import { Firestore, getFirestore } from "firebase/firestore";
 
 const app: FirebaseApp = initializeApp({
-  apiKey: "AIzaSyBuYMzWxuZCUrnZganpG8p2xCAnMzPGxVY",
-  authDomain: "radio-sulamita-api.firebaseapp.com",
-  projectId: "radio-sulamita-api",
-  storageBucket: "radio-sulamita-api.appspot.com",
-  messagingSenderId: "975912397385",
-  appId: "1:975912397385:web:c888b9f3f682819f532d98",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 });
 
 const db: Firestore = getFirestore(app);
